2af5af555a8c11bcc70b7a5e6b9ea08c607ee402
TacosyHorchata <priosabarca@hotmail.com>
2024-01-25 21:13:01 -0600

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,14 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 import 'firebase/storage';
 
-const firebaseConfig = {};
+const firebaseConfig = {
+    apiKey: "AIzaSyBkVJAAoh7fHe0RvqiJe5U_pJU0Ae8ftns",
+    authDomain: "makers-hackathon.firebaseapp.com",
+    projectId: "makers-hackathon",
+    storageBucket: "makers-hackathon.appspot.com",
+    messagingSenderId: "1020189953659",
+    appId: "1:1020189953659:web:efa0d16891eda273a6dcde"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
